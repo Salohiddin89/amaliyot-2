69048a17d701c8e73c3083413f1d92dfa8a5a199
Salohiddin89 <axmadjonovsalohidddin97@gmail.com>
2025-12-26 16:46:39 +0500

    done for server

--- website/settings.py
+++ website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-o5dq=s56zhi-n*-_zdkqq7g#2!7%v)enrn$ufe8srdx_$ttmn2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
